44d6a116230a5463eb515e3b820575f011032472
homelander007 <115377742+Ankush1505@users.noreply.github.com>
2025-12-25 19:49:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
 from passlib.context import CryptContext
 
-# 1. Tell passlib we want to use "bcrypt" (the industry standard algorithm)
+#  we want to use "bcrypt"
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# 2. Function to Hash a password
+# Hash a password
 def hash(password: str):
     return pwd_context.hash(password)
 
-# 3. Function to Verify a password (we will use this later for Login)
+# Function to Verify a password
 def verify(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
